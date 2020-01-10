9701b2717dd21464ad2aea78d08ef8f1600434c5
Hemanth Dinesh <hendish@icloud.com>
2020-01-09 18:01:33 -0800

    Percentage Calculator

---
+++ src/PercentageCalculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class PercentageCalculator {
+    public static void main(String[] args) {
+        Scanner input = new  Scanner(System.in);
+        System.out.println("Enter a number");
+        int number;
+        number = input.nextInt();
+        System.out.println("Enter the percentage");
+        float percentage;
+        percentage = input.nextInt();
+        float answer;
+        answer = (percentage * number);
+        System.out.println(answer/100);
+
+    }
+}
